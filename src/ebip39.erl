75203401b39aeba45f20b7128f84eeda6a2a4ab6
Hans Svensson <hanssv@gmail.com>
2022-01-27 08:24:26 +0100

    Add/export validation of Mnemonic

--- src/ebip39.erl
+++ src/ebip39.erl
@@ -8,23 +8,48 @@
 -endif.
 
 -export([generate_mnemonic/1,
-         mnemonic_to_seed/2]).
+         mnemonic_to_seed/2,
+         validate_mnemonic/1]).
 
 -define(WORDLIST, "wordlist_en.txt").
 
+-spec mnemonic_to_seed(Mnemonic :: binary(),
+                       PassPhrase :: iodata()) -> <<_:512>>.
 mnemonic_to_seed(Mnemonic, PassPhrase) ->
-  verify_mnemonic(Mnemonic),
+  assert_mnemonic(Mnemonic),
   {ok, BinSeed} = epbkdf2:pbkdf2(sha512,
                                  Mnemonic,
                                  iolist_to_binary(["mnemonic", PassPhrase]),
                                  2048),
   BinSeed.
 
+-spec generate_mnemonic(Size :: non_neg_integer()) -> binary().
 generate_mnemonic(Size) ->
   case Size rem 32 of
     0 -> gen_mnemonic(Size);
     _ -> error({bad_mnemonic_size, Size, not_a_multiple_of_32})
   end.
+
+-spec validate_mnemonic(Mnemonic :: binary()) -> ok | {error, Reason :: atom()}.
+validate_mnemonic(Mnemonic) ->
+  Table = word_table(),
+  Ixs   = [ maps:get(W, Table) || W <- string:lexemes(Mnemonic, " ") ],
+  Size  = length(Ixs) * 11,
+  CSize = Size rem 32,
+  SSize = Size - CSize,
+  <<Seed:(SSize div 8)/bytes, CS:(CSize)>> = << <<Ix:11>> || Ix <- Ixs >>,
+  case crypto:hash(sha256, Seed) of
+    <<CS:(CSize), _/bitstring>> -> ok;
+    _                           -> {error, checksum_error}
+  end.
+
+%% --- Internal functions
+assert_mnemonic(Mnemonic) ->
+  case validate_mnemonic(Mnemonic) of
+    ok -> ok;
+    {error, R} -> error({bad_mnemonic_string, R})
+  end.
+
 
 gen_mnemonic(Size) ->
   gen_mnemonic(Size, crypto:strong_rand_bytes(Size div 8)).
@@ -35,18 +60,6 @@ gen_mnemonic(Size, Seed) ->
   Table  = ix_table(),
   Words = [ maps:get(G, Table) || <<G:11>> <= <<Seed/bytes, CS:(CSize)>> ],
   iolist_to_binary(lists:join(<<" ">>, Words)).
-
-verify_mnemonic(Mnemonic) ->
-  Table = word_table(),
-  Ixs   = [ maps:get(W, Table) || W <- string:lexemes(Mnemonic, " ") ],
-  Size  = length(Ixs) * 11,
-  CSize = Size rem 32,
-  SSize = Size - CSize,
-  <<Seed:(SSize div 8)/bytes, CS:(CSize)>> = << <<Ix:11>> || Ix <- Ixs >>,
-  case crypto:hash(sha256, Seed) of
-    <<CS:(CSize), _/bitstring>> -> ok;
-    _                           -> error({bad_mnemonic_string, checksum_error})
-  end.
 
 word_table() ->
   Words = read_wordlist(),
